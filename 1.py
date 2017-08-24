89963b84c8e76bbfa7eeb4c102ef431e5ea75f38
akhilpatelpatlolla <akhilpatlolla@gmail.com>
2017-08-24 18:09:15 +0530

    Updated upto part -2

--- 1.py
+++ 1.py
@@ -1,47 +1,37 @@
-file1=open("file1.txt").read().lower().replace(".","").replace(",","").replace("!","").replace("@","").replace("#","").replace("$","").replace("%","").replace("^","").replace("&","").replace("*","").replace("(","").replace(")","").split()
-# print(file1)
-file2=open("file2.txt").read().lower().replace(".","").replace(",","").replace("!","").replace("@","").replace("#","").replace("$","").replace("%","").replace("^","").replace("&","").replace("*","").replace("(","").replace(")","").split()
-# print(file2)
-# s=['!','@','#','$','%','^','&','*','(',')','-','=','+','[',']','\',':',';','"','']
+file1=open("file1.txt").read()
+file2=open("file2.txt").read()
+import re
+file1 = re.sub('[^a-zA-Z0-9_\n\.\ ]', ' ', file1)
+file1 = re.sub(' +',' ',file1)
+file2 = re.sub('[^a-zA-Z0-9_\n\.]', ' ',file2)
+file2 = re.sub(' +',' ',file2)
+file1=file1.lower().replace(".","").replace(",","").split()
+file2=file2.lower().replace(".","").replace(",","").split()
 dict1={}
-dict2={}
+dict2={}											
 for word in file1:
 	if word in dict1:
 		dict1[word]+=1
 	else:
 		dict1[word]=1
-print(dict1)	
 for word in file2:
 	if word in dict2:
 		dict2[word]+=1
 	else:
 		dict2[word]=1
-print(dict2)
 sum=0
 for i in dict1.keys():
 	for j in dict2.keys():
 		if i==j:
 			c=dict1[i]*dict2[j]
-			print(c)
 			sum=c+sum
-print(sum)
 sum1=0
 sum2=0
 for i in dict1.keys():
 	sum1=sum1+(dict1[i]**2)
-print(sum1)
 for i in dict2.keys():
 	sum2=sum2+(dict2[i]**2)
-print(sum2)
 import math
 sum3=math.sqrt(sum2)*math.sqrt(sum1)
-print(sum3)
 sum4=(sum/sum3)*100
-print(sum4)		
-
-# import json
-# dict1 = "{'akhil' : 'patel', 'patlolla' : 'msit'}"
-# print(type(dict1))
-# json_acceptable_string = dict1.replace("'", "\"")
-# dict1 = json.loads(json_acceptable_string)
-# print(type(dict1))
+print(sum4)	
---
+++ BagOfWords.py
@@ -0,0 +1,58 @@
+import os.path
+import math
+path=input("Enter the Directory Name:")
+file=os.listdir(path)
+print(file)
+os.chdir(path)
+def PgChecker(file1,file2):
+    def Characters(s):
+        s=s.lower()
+        c="abcdefghijklmnopqrstuvxxyz_0123456789 "
+        for i in s:
+            if i not in c:
+                s=s.replace(i," ")
+        s=s.replace("\n"," ").replace("\xa0"," ")
+        s=s.split(" ")
+        return s
+    s1=Characters(file1.read())
+    s2=Characters(file2.read())    
+    def Dict(s,d):
+        for i in s:
+            if i==' ' or i=='':
+                pass
+            elif i not in d:
+                d[i]=1
+            else:
+                d[i]+=1 
+        return d
+    d1=Dict(s1,{})
+    d2=Dict(s2,{})    
+    def DotproductofNum(d1,d2):
+        L=[]
+        for i in d1:
+            for j in d2:
+                if i==j:
+                    L.append(d1[i]*d2[j])
+        return L
+    numerator=sum(DotproductofNum(d1,d2))
+    def DotproductofDen(d):
+        dum=0
+        for i in d:
+            dum+=(d[i])**2
+        dum=(dum)**(1/2)
+        return dum
+    denominator=(DotproductofDen(d1))*(DotproductofDen(d2))
+    return round((numerator/denominator)*100)
+list1=[]
+for i in range (len(file)):
+    for j in range (len(file)):
+        if i==j:
+            list1.append("Nil")
+        else:
+            file1=open(file[i],"r")
+            file2=open(file[j],"r")
+            list1.append(PgChecker(file1,file2))
+            file1.close()
+            file2.close()
+    print(file[i],list1,"\n")
+    list1=[]
